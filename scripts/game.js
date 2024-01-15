50fa63d99be7217629ee03d995662f5f59ad7578
Sagittarius87 <Jarik-87@yandex.ru>
2024-01-15 16:24:08 +0300

    Сделал вывод изображения на canvs. Написал реализацию методов start() и run() для подгрузки фона

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,23 @@
 let game = {
     start() {
+        this.canvas = document.getElementById('canvas')
+        this.ctx = this.canvas.getContext('2d')
+
+        this.background = new Image()
+        this.background.src = 'sprites/background.png'
+
+        this.background.addEventListener('load', () => {
+            this.run()
+        })
+
         console.log('start')
+        console.log(this.background.src)
     },
     run() {
+        window.requestAnimationFrame(() => {
+            this.ctx.drawImage(this.background, 0, 0)
+        })
+
         console.log('run')
     }
 }
